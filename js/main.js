05d22a180a644f6691bc35d82218346e40ad49b3
yoboy999 <tsanyeo@tsanyeos-MacBook-Pro.local>
2016-02-22 18:00:52 +1100

    add remove.Class()

--- js/main.js
+++ js/main.js
@@ -8,23 +8,23 @@ $(document).ready(function() {
 
     // if the user inputs New York City / NYC / New York change the body class to 'nyc'
         if (city == 'New York' || city == 'New York City' || city == "NYC") {
-            $('body').addClass('nyc');
+            $('body').removeClass().addClass('nyc');
         }
     // if the user inputs SF/San Francisco/Bay Area change the body class to 'sf'
         if (city == 'SF' || city == 'San Francisco' || city == 'Bay Area') {
-            $('body').addClass('sf');
+            $('body').removeClass().addClass('sf');
         }
     // if the user inputs Austin/ATX change the body class to 'austin'
         if (city == 'Austin' || city == 'ATX') {
-            $('body').addClass('austin');
+            $('body').removeClass().addClass('austin');
         }
     // if the user inputs Los Angeles/LA/LAX change the body class to 'la'
         if (city == 'Los Angeles' || city == 'LA' || city == 'LAX') {
-            $('body').addClass('la');
+            $('body').removeClass().addClass('la');
         }
     // if the user inputs Sydney/SYD change the body class to 'sydney'
         if (city == 'Sydney' || city == 'SYD') {
-            $('body').addClass('sydney');
+            $('body').removeClass().addClass('sydney');
         }
     });
                       
